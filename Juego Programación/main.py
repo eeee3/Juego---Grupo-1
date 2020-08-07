9ca11ce849a23030bc06179fd257b0ed91f1c062
nicobartok0 <61692341+nicobartok0@users.noreply.github.com>
2020-08-07 09:57:11 -0300

    Add files via upload

--- Juego Programación/main.py
+++ Juego Programación/main.py
@@ -2,6 +2,14 @@ import pygame
 
 pygame.init()
 
+#Definimos los colores
+
+NEGRO = (0, 0, 0)
+BLANCO = (255, 255, 255)
+ROJO = (255, 0, 0)
+VERDE = (0, 255, 0)
+
+
 #Definimos eltamaño de la pantalla
 
 pantalla = pygame.display.set_mode([400, 400])
@@ -28,17 +36,44 @@ hecho = False
 class Personaje(pygame.sprite.Sprite):
     def __init__(self, personaje_x, personaje_y):
         super().__init__()
-        self.image = pygame.image.load("main-1.png").convert()
+        self.bucle_anim = 0
+        self.movimiento = 0
+        self.image = pygame.image.load("stand.bmp").convert()
+        self.image.set_colorkey(BLANCO)
         self.personaje_x = 0
         self.personaje_y = 0
-        
         self.rect = self.image.get_rect()
         self.rect.x = personaje_x
         self.rect.y = personaje_y
 
+    def update(self):
+        if self.bucle_anim == 1:
+            self.movimiento += 1
+            if self.movimiento == 1:
+                self.image = pygame.image.load("main-walk-1.bmp").convert()
+            elif self.movimiento == 2:
+                self.image = pygame.image.load("main-walk-2.bmp").convert()
+            elif self.movimiento == 3:
+                self.image = pygame.image.load("main-walk-3.bmp").convert()
+            elif self.movimiento == 4:
+                self.image = pygame.image.load("main-walk-4.bmp").convert()
+            elif self.movimiento == 5:
+                self.image = pygame.image.load("main-walk-5.bmp").convert()
+            elif self.movimiento == 6:
+                self.image = pygame.image.load("main-walk-6.bmp").convert()
+            elif self.movimiento == 7:
+                self.image = pygame.image.load("main-walk-7.bmp").convert()
+            else:
+                self.movimiento = 1
+        else:
+            self.image = pygame.image.load("stand.bmp").convert()
+
+        
+
 #Creamos un objeto protagonista de la clase del personaje.
 
-protagonista = Personaje(200,200)
+protagonista = Personaje(170,200)
+
 
 #Añadimos los sprites a las listas correspondientes.
 
@@ -58,6 +93,7 @@ while not hecho:
                 offset_x = -1
             if evento.key == pygame.K_LEFT:
                 offset_x = 1
+                protagonista.bucle_anim +=1
             if evento.key == pygame.K_DOWN:
                 offset_y = -1
             if evento.key == pygame.K_UP:
@@ -65,6 +101,7 @@ while not hecho:
         if evento.type == pygame.KEYUP:
             offset_x = 0
             offset_y = 0
+            protagonista.bucle_anim = 0
 
 #Definimos el offset del movimiento de la pantalla definido anteriormente.
 
@@ -73,8 +110,12 @@ while not hecho:
 
     # Copia la imagen en pantalla:
     pantalla.blit(imagen_de_fondo, [x, y])
+    
+    listade_todoslos_sprites.update()
+
     listade_todoslos_sprites.draw(pantalla)
     
+
     pygame.display.flip()
 
     reloj.tick(60)
